efc8c57115f6e4f9b993bf74b6571d2d530ed8d7
Samarth Asthana <samarth.asthana@gmail.com>
2016-01-10 22:13:01 -0600

    Refactored self.search

--- app/models/book.rb
+++ app/models/book.rb
@@ -29,32 +29,32 @@ class Book < ActiveRecord::Base
   end
 
   def self.search(query, options = {title_only: false, book_format_type_id: nil, book_format_physical: nil })
-    @combined_results = []
+    combined_results = []
     unless query.nil?
-      @author_results = []
-      @publisher_results = []
-      @title_results = self.where("lower(title) like ?","%#{query.downcase}%").to_a
+      author_results = []
+      publisher_results = []
+      title_results = self.where("lower(title) like ?","%#{query.downcase}%").to_a
       if options[:title_only]
-        return (@title_results.uniq).sort_by {|b| -b.average_rating}
+        # Returning results just for title search in desc order of rating
+        return (title_results.uniq).sort_by { |b| -b.average_rating }
       end
-      Author.where("lower(last_name) = ?", query.downcase).each do |a|
-        @author_results += Book.where("author_id = ?", a.id)
-      end
-      Publisher.where("lower(name) = ?", query.downcase).each do |p|
-        @publisher_results += Book.where("publisher_id = ?", p.id)
-      end
-      @combined_results = ((@author_results + @publisher_results + @title_results).uniq).sort_by { |b| -b.average_rating }
+      # Getting results based on author last_name
+      Author.where("lower(last_name) = ?", query.downcase).each { |a| author_results += Book.where("author_id = ?", a.id) }
+      # Getting results based on publisher name
+      Publisher.where("lower(name) = ?", query.downcase).each { |p| publisher_results += Book.where("publisher_id = ?", p.id) }
+      # Combining the result arrays and soring by average rating in Desc order
+      combined_results = ((author_results + publisher_results + title_results).uniq).sort_by { |b| -b.average_rating }
 
       unless options[:book_format_type_id].nil? and options[:book_format_physical].nil?
-        @physical_formats = []
+        physical_formats = []
         if options[:book_format_physical]
-          BookFormatType.where(:physical => true).each {|bft| @physical_formats << bft.id}
+          BookFormatType.where(:physical => true).each {|bft| physical_formats << bft.id}
         else
-          @physical_formats << options[:book_format_type_id]
+          physical_formats << options[:book_format_type_id]
         end
-        @combined_results.reject! {|b| BookFormat.where(:book_id => b.id, :book_format_type_id => @physical_formats).empty?}
+        combined_results.reject! {|b| BookFormat.where(:book_id => b.id, :book_format_type_id => physical_formats).empty?}
       end
     end
-    @combined_results
+    combined_results
   end
 end
